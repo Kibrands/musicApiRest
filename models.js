f8819da07ef234302307008b5ddc249cb924a0c0
Kibrands <jjextreme13@gmail.com>
2020-02-21 13:31:19 +0100

    cover added to models

--- models.js
+++ models.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
-const Artist = mongoose.model('Artist', { name: String, lastName: String, nick: String });
+const Artist = mongoose.model('Artist',
+    new mongoose.Schema({ name: String, lastName: String, nick: String })
+);
 
 const Song = mongoose.model('Song',
-    new mongoose.Schema({ title: String, price: Number, label: String, releaseDate: Date })
+    new mongoose.Schema({ title: String, price: Number, label: String, releaseDate: Date, cover: String })
 );
 
 module.exports = {
